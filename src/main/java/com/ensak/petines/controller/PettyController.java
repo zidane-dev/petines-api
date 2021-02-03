4077755693d33284e3216379cf32f8267215a93f
yassine <ysnowtech@gmail.com>
2021-02-03 14:00:26 +0100

    adjust petties controller and service

---
+++ src/main/java/com/ensak/petines/controller/PettyController.java
@@ -0,0 +1,56 @@
+package com.ensak.petines.controller;
+
+import com.ensak.petines.model.Pets;
+import com.ensak.petines.repositories.PettyRepository;
+import com.ensak.petines.services.PettyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PettyController {
+    @Autowired
+    PettyRepository pettyRepository;
+
+    @Autowired
+    PettyService pettyService;
+
+    @RequestMapping("/petties")
+    public List<Pets> getAllPets()    {
+        return pettyRepository.findAll();
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/petties")
+    @ResponseBody
+    public void addPetty(@RequestBody Pets pets) {
+        pettyRepository.save(pets);
+    }
+
+    @PostMapping("/petties/{Id}")
+    public void updatePetty(@PathVariable int Id, @RequestBody Pets petty ) {
+
+        pettyService.updatePetty(Id, petty);
+    }
+
+    @DeleteMapping("/petties/delete/{Id}")
+    public void deletePetty(@PathVariable int Id) {
+        pettyService.deletePetty(Id);
+    }
+
+    /*
+    @RequestMapping("/pets/{productId}")
+    public Pets getProduct(@PathVariable Integer productId){
+        return pettyRepository.findById(productId);
+    }
+
+ */
+
+/*
+    @PutMapping("/products")
+    public void updateProducts(@RequestBody OrderItemWrapper orderItemWrapper ) {
+        pettyService.updatePets(orderItemWrapper);
+    }
+ */
+
+}
