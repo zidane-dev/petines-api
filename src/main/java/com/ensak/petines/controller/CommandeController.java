2d8a0ef2f9b7a94a3cdb94378af93b4eb7cf01b7
salmalrch <salmalrch@gmail.com>
2021-02-14 17:27:47 +0100

    finished add commande

--- src/main/java/com/ensak/petines/controller/CommandeController.java
+++ src/main/java/com/ensak/petines/controller/CommandeController.java
@@ -1,9 +1,12 @@
 package com.ensak.petines.controller;
 
 import com.ensak.petines.model.Commande;
+import com.ensak.petines.model.Pets;
 import com.ensak.petines.model.User;
 import com.ensak.petines.repositories.CommandeRepository;
 import com.ensak.petines.repositories.UserRepository;
+import com.ensak.petines.services.CommandeService;
+import com.ensak.petines.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +21,13 @@ public class CommandeController {
     CommandeRepository commandeRepository;
 
     @Autowired
+    CommandeService commandeService;
+
+    @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
 
 
 
@@ -34,9 +43,18 @@ public class CommandeController {
         return commandeRepository.findCommandeByUser(user);
     }
 
-    @RequestMapping(method= RequestMethod.POST, value="/livraison")
+   /* @RequestMapping(method= RequestMethod.POST, value="/livraison")
     public void addCommande(@RequestBody Commande commande) {
         commandeRepository.save(commande);
+    }*/
+
+    @RequestMapping(method= RequestMethod.POST, value="/commandes/add/{username}")
+    public Commande addCommande(@PathVariable String username, @RequestBody Commande commande) {
+        User u = userRepository.findByUsername(username);
+       return commandeService.addCommande(u,commande);
+
+
     }
+
 
 }
--- src/main/java/com/ensak/petines/model/Pets.java
+++ src/main/java/com/ensak/petines/model/Pets.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name="petty")
+@Table(name="pets")
 public class Pets {
 
     @Id
