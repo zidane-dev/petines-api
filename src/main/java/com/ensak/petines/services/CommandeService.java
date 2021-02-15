42d60bcc439dd98e75fb5a6e5f97c53bf7bf32c5
yassine <ysnowtech@gmail.com>
2021-02-15 10:00:11 +0100

    Commande part attached and tested

--- src/main/java/com/ensak/petines/services/CommandeService.java
+++ src/main/java/com/ensak/petines/services/CommandeService.java
@@ -1,22 +1,30 @@
 package com.ensak.petines.services;
 
 import com.ensak.petines.model.Commande;
+import com.ensak.petines.model.Pets;
 import com.ensak.petines.model.User;
 import com.ensak.petines.repositories.CommandeRepository;
+import com.ensak.petines.repositories.PettyRepository;
 import com.ensak.petines.repositories.UserRepository;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.activation.CommandMap;
 import java.util.List;
 
 @Service
 public class CommandeService {
+
 
     @Autowired
     CommandeRepository commandeRepository;
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    PettyRepository pettyRepository;
 
 
 
@@ -27,6 +35,21 @@ public class CommandeService {
     public List<Commande> getCommandesByUser(User user) {
 
         return   commandeRepository.findCommandeByUser(user);
+    }
+
+    public Commande addCommande( User user, Commande commande)
+    {
+        Commande c = new Commande();
+        c.setUser(user);
+        c.setPet(commande.getPet());
+        c.setDate(commande.getDate());
+        c.setLocation(commande.getLocation());
+        c.setStatus(commande.getStatus());
+
+        commandeRepository.save(c);
+        return c;
+
+
     }
 
 }
